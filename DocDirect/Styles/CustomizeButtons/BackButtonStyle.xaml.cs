790699108569f7c926c5340905b73bb07a258da4
Nikolay Bogdanov <rjkz161@gmail.com>
2015-01-01 22:44:21 +0300

    Add button Back for pages and minor fixes

---
+++ DocDirect/Styles/CustomizeButtons/BackButtonStyle.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using System.Collections;
+
+namespace DocDirect.Styles.CustomizeButtons
+{
+    
+}
